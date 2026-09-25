628b09295580597a1719f2e4b1f7340928668c04
UvA Programming Lab <terra@proglab.nl>
2026-09-25 17:46:48 +0200

    Update weken.py

--- weken.py
+++ weken.py
@@ -17,5 +17,5 @@ def weeks_elapsed(day1: int, day2: int) -> int:
     if __name__ == '__main__':
         day1 = int(input("Dagnummer 1: "))
         day2 = int(input("Dagnummer 2: "))
-        result = weeks_elapsed
+        result = weeks_elapsed()
         print(f"Er zijn {result} volle weken verstreken.")
