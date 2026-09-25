7fd0c762dbab9f7ed8fe5482758e20d92c20aa37
UvA Programming Lab <terra@proglab.nl>
2026-09-25 18:03:12 +0200

    Update weken.py

--- weken.py
+++ weken.py
@@ -1,6 +1,9 @@
 def weeks_elapsed(day1: int, day2: int) -> int:
     return abs(day1-day2)//7
     """
+    day1 and day2 are days in the same year. Return the number of full weeks
+    that have elapsed between the two days.
+    
     >>> weeks_elapsed(3, 20)
     2
     >>> weeks_elapsed(20, 3)
